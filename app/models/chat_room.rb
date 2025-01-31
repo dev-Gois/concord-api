da78941cf75151295d45e0b0994316cd8bf406aa
Hivna <hivna12@gmail.com>
2025-01-31 00:13:57 -0300

    fix: adjust RuboCop configuration and fix issues

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,5 +1,5 @@
 class ChatRoom < ApplicationRecord
-    enum chat_type: { private_chat: 0, group_chat: 1 } 
+    enum chat_type: { private_chat: 0, group_chat: 1 }
 
     validates :name, presence: true
     validates :chat_type, presence: true
--- spec/models/chat_room_spec.rb
+++ spec/models/chat_room_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ChatRoom, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name)}
-    it { should validate_presence_of(:chat_type)}
-    it { should define_enum_for(:chat_type).with_values([:private_chat, :group_chat])}
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:chat_type) }
+    it { should define_enum_for(:chat_type).with_values([ :private_chat, :group_chat ]) }
   end
 end
