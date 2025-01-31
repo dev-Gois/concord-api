50bdd433a4c5d4eeecc0218ae1f2e1c921785228
Matheus Gois <mattheusgoiis@gmail.com>
2025-01-30 23:52:48 -0300

    feat: implement login service

---
+++ app/services/users/interactors/find.rb
@@ -0,0 +1,23 @@
+module Users
+  module Interactors
+    class Find
+      include Interactor
+
+      delegate :user_params, to: :context
+
+      def call
+        raise "Email is required" unless user_params[:email].present?
+        raise "User not found" unless find_user
+        context.user = find_user
+      rescue StandardError => e
+        context.fail!(message: e.message)
+      end
+
+      private
+
+      def find_user
+        @find_user ||= User.find_by(email: user_params[:email])
+      end
+    end
+  end
+end
