92d0a52db643996df1be90c798580f29a195c96d
Matheus Gois <mattheusgoiis@gmail.com>
2025-01-30 23:52:57 -0300

    feat: implement action/route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   def register
-    result = Users::Organizers::Register.call(user_params: user_params)
+    result = Users::Organizers::Register.call(user_params: register_params)
+    if result.success?
+      render json: UserSerializer.new(result.user).serializable_hash.to_json, status: :created
+    else
+      render json: { errors: result.message }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    result = Users::Organizers::Login.call(user_params: login_params)
     if result.success?
       render json: UserSerializer.new(result.user).serializable_hash.to_json, status: :created
     else
@@ -10,7 +19,11 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
+  def register_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def login_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   post "register", to: "users#register"
+  post "login", to: "users#login"
 end
