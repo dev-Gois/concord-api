cfabf96118f0d39ade651df9d46402279a5d71dc
Matheus Gois <mattheusgoiis@gmail.com>
2025-02-18 12:47:18 -0300

    return created at

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,7 +1,7 @@
 class MessageSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :content, :chat_room_id
+  attributes :id, :content, :chat_room_id, :created_at
 
   attribute :user do |message|
     UserSerializer.new(message.user).serializable_hash[:data][:attributes]
